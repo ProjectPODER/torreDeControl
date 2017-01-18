fde0da14fef4972c5a1c0cbcaeecce72de5381cf
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2017-01-18 18:29:38 -0300

    modify nodes on the fly

--- src/main.js
+++ src/main.js
@@ -6,14 +6,17 @@ import Filter from './filters.js';
 import MathSet from './sets.js';
 require('./css/main.scss');
 
-const nodes = [
+let nodes = [
 	// { id: 'desktop', name: 'Cytoscape', value: 100, href: 'http://cytoscape.org' },
  //    { id: 'js', name: 'Cytoscape.js', value: 50, href: 'http://js.cytoscape.org' }
 ];
 
-const links = [
+let links = [
 	// { source: 'desktop', target: 'js' }
 ];
+
+let node;
+let link;
 
 const AppData = {
 	persons: [],
@@ -22,32 +25,40 @@ const AppData = {
 };
 
 $(() => {
-	getData((data) => {
+	getData(data => {
 		AppData.organizations = data.organizations,
 		AppData.contracts = data.contracts;
-		console.log(AppData.contracts);
 		const contractsAmount = getContractsAmount(AppData.contracts);
 		const contractsByTypes = getContractsByTypes(AppData.contracts);
-		
-		const node = { id: 'contracts', name: 'contracts', value: contractsAmount / 1000000000, href: 'http://cytoscape.org', group: 1 };
+		const slidesObjects = [	null,
+			{nodes: [], links: []},
+			{nodes: [], links: []},
+			{nodes: [], links: []}
+		]; 
+		const node = { id: 'contracts', name: 'contracts', value: contractsAmount / 1000000000, type: 'all', group: 1 };
+		slidesObjects[1].nodes.push(node);
 		nodes.push(node);
 		for (let i in contractsByTypes) {
 			const contractByType = contractsByTypes[i];
-			const node = { id: contractByType.name, name: contractByType.name, value: contractByType.amount / 1000000000, href: 'http://cytoscape.org', group: 2 };
-			const link = { source: 'contracts', target: contractByType.name };
-			console.log(contractByType.amount)
+			const node = { id: contractByType.name, name: contractByType.name, value: contractByType.amount / 1000000000, type: 'contract_type', group: 2 };
+			const link = { source: 'contracts', target: contractByType.name, type: 'contract_type', distance: 100 };
+			slidesObjects[2].nodes.push(node);
+			slidesObjects[2].links.push(link);
 			nodes.push(node);
 			links.push(link);
 			for (let j in contractByType.contracts) {
 				const contract = contractByType.contracts[j];
-				const node = { id: contract.ocid, name: contract.amount, value: Math.log(contract.amount) / 10, href: 'http://cytoscape.org', group: 3 };
-				const link = { source: contractByType.name, target: contract.ocid };
+				const node = { id: contract.ocid, name: contract.amount, value: Math.log(contract.amount) / 10, type: 'contract', group: 3 };
+				const link = { source: contractByType.name, target: contract.ocid, type: 'contract', distance: 20 };
+				slidesObjects[3].nodes.push(node);
+				slidesObjects[3].links.push(link);
 				nodes.push(node);
 				links.push(link);
 			}
 		}
-		console.log(nodes, links)
-		draw();
+		nodes = [...slidesObjects[1].nodes, ...slidesObjects[2].nodes, ...slidesObjects[3].nodes];
+		links = [...slidesObjects[1].links, ...slidesObjects[2].links, ...slidesObjects[3].links];
+		setupD3();
 	});
 	
 	
@@ -58,158 +69,159 @@ function getData(cb) {
 		getContracts,
 		getOrganizations
 	], (err, results) => {
-		console.log(results)
 		cb({
 			contracts: results[0],
 			organizations: results[1]
-		})
+		});
 	})
 }
 
 function getPersons(cb) {
 	$.get('https://www.quienesquien.wiki/api/v1/persons')
-	.done((response) => {
-		cb(null, response.data);
-	});
+	.done(response => {cb(null, response.data);});
 }
 
 function getOrganizations(cb) {
-	// const contracts = new MathSet(contractsData);
-	// const companyFilter = new Filter({property: 'proveedor'});
-	// const organizations = Object.keys(contracts.countByFilterProperty(companyFilter));
-	// const query = `?names=${htmlEncode(organizations.join(','))}`;
-	const query = '';
-	$.get(`https://www.quienesquien.wiki/api/v1/organizations${query}`)
-	.done((response) => {
-		console.log(response.data);
-		cb(null, response.data);
-	});
+	$.get('https://www.quienesquien.wiki/api/v1/organizations')
+	.done(response => {cb(null, response.data);});
 }
 
 function getContracts(cb) {
 	$.get('https://www.quienesquien.wiki/api/v1/contracts?dependency=Grupo%20Aeroportuario%20De%20La%20Ciudad%20De%20M%C3%A9xico,%20S.A.%20de%20C.V.')
-	.done((response) => {
-		cb(null, response.data);
-	});
+	.done(response => {cb(null, response.data);});
 }
 
-function draw() {
-	var svg = d3.select("svg"),
-	width = +svg.attr("width"),
-	height = +svg.attr("height");
+function setupD3() {
+	const svg = d3.select("svg");
+	const width = +svg.attr("width");
+	const height = +svg.attr("height");
+	const color = d3.scaleOrdinal(d3.schemeCategory20);
 
-	var color = d3.scaleOrdinal(d3.schemeCategory20);
+	const simulation = d3.forceSimulation()
+	    .force("charge", d3.forceManyBody().strength(-20))
+	    .force("link", d3.forceLink().id(d => d.id).distance(d => d.distance).strength(1))
+	    .force("center", d3.forceCenter(width / 2, height / 2))
+	    .on("tick", ticked);;
 
-	var simulation = d3.forceSimulation()
-	    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-	    .force("charge", d3.forceManyBody())
-	    .force("center", d3.forceCenter(width / 2, height / 2));
+	const slide_1 = new Filter({property: 'type', operator: 'eq', expected: 'all'});
+	const slide_2 = new Filter({property: 'type', operator: 'eq', expected: 'contract_type'});
+	const slide_3 = new Filter({property: 'type', operator: 'eq', expected: 'contract'});
 
+	document.addEventListener("keydown", keyboard);
 
-	var graph = {
-  		nodes,
-  		links
-    };
-
-	var link = svg.append("g")
-	  .attr("class", "links")
-	.selectAll("line")
-	.data(graph.links)
-	.enter().append("line")
-	  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-	var node = svg.append("g")
-	  .attr("class", "nodes")
-	.selectAll("circle")
-	.data(graph.nodes)
-	.enter().append("circle")
-	  .attr("r", (d) => {
-	  	return d.value * 3;
-	  })
-	  .attr("fill", function(d) { return color(d.group); })
-	  .call(d3.drag()
-	      .on("start", dragstarted)
-	      .on("drag", dragged)
-	      .on("end", dragended));
-
-	node.append("title")
-	  .text(function(d) { return d.id; });
-
-	simulation
-	  .nodes(graph.nodes)
-	  .on("tick", ticked);
-
-	simulation.force("link")
-	  .links(graph.links);
+	let g = svg.append("g");
+	link = g.append("g").selectAll('link');
+	node = g.append("g").selectAll('node');
 
 	function ticked() {
-	link
-	    .attr("x1", function(d) { return d.source.x; })
-	    .attr("y1", function(d) { return d.source.y; })
-	    .attr("x2", function(d) { return d.target.x; })
-	    .attr("y2", function(d) { return d.target.y; });
+		node
+		    .attr("cx", d => d.x)
+		    .attr("cy", d => d.y);
 
-	node
-	    .attr("cx", function(d) { return d.x; })
-	    .attr("cy", function(d) { return d.y; });
+		link
+		    .attr("x1", d => d.source.x)
+		    .attr("y1", d => d.source.y)
+		    .attr("x2", d => d.target.x)
+		    .attr("y2", d => d.target.y);
 	}
 
+	function draw(graph) {
+		node = node.data(graph.nodes);
+		node.exit().remove();
+		node = node.enter().append("circle")
+			.attr("r", d => d.value * 3)
+			.attr("fill", d => color(d.group))
+			.attr("class", "nodes")
+			.merge(node);
 
-	function dragstarted(d) {
-	  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-	  d.fx = d.x;
-	  d.fy = d.y;
+		node.each(d => {
+			if (d.type == 'all') {
+				d.fx = d.x;
+				d.fy = d.y;
+			}
+		});
+
+		link = link.data(graph.links);
+		link.exit().remove();
+		link = link.enter().append("line")
+			.attr("class", "links")
+			.merge(link);
+
+		// node.append("title")
+		// 	.text(d => d.name);
+
+		simulation.nodes(graph.nodes);
+		simulation.force("link").links(graph.links);
+		simulation.alpha(0.1).restart();
 	}
 
-	function dragged(d) {
-	  d.fx = d3.event.x;
-	  d.fy = d3.event.y;
+	function keyboard(a) {
+		let graph;
+		
+		switch (a.key) {
+			case 'a':
+				graph = {
+					nodes: objectToArray((new MathSet(nodes)).filter(slide_1).toObject()),
+					links: objectToArray((new MathSet(links)).filter(slide_1).toObject())
+				};
+				node.each(d => {
+					d.fx = null;
+					d.fy = null;
+					if (d.type == 'all') {
+						d.fx = d.x;
+						d.fy = d.y;
+					} else {
+						d.x = 0;
+						d.y = 0;
+					}
+				});	
+				break;
+			case 's':
+				graph = {
+					nodes: objectToArray((new MathSet(nodes)).filter(slide_1, slide_2).toObject()),
+					links: objectToArray((new MathSet(links)).filter(slide_1, slide_2).toObject())
+				};
+				node.each(d => {
+					d.fx = null;
+					d.fy = null;
+					if (d.type == 'all' || d.type == 'contract_type') {
+						d.fx = d.x;
+						d.fy = d.y;
+					} else {
+						d.x = 0;
+						d.y = 0;
+					}
+				});	
+				break;
+			case 'd':
+				graph = {
+					nodes: objectToArray((new MathSet(nodes)).filter(slide_1, slide_2, slide_3).toObject()),
+					links: objectToArray((new MathSet(links)).filter(slide_1, slide_2, slide_3).toObject())
+				};
+				node.each(d => {
+					d.fx = null;
+					d.fy = null;
+					if (d.type == 'all' || d.type == 'contract_type' || d.type == 'contract') {
+						d.fx = d.x;
+						d.fy = d.y;
+					} else {
+						d.x = 0;
+						d.y = 0;
+					}
+				});	
+				break;
+		}
+		
+		draw(graph);
 	}
-
-	function dragended(d) {
-	  if (!d3.event.active) simulation.alphaTarget(0);
-	  d.fx = null;
-	  d.fy = null;
-	}
-
-
-
-
-	// var cy = Cytoscape({
-	//   container: $('.container')[0],
-	//   style: [
-	//     {
-	//       selector: 'node',
-	//       style: {
-	//         width: 'data(value)',
-	//         height: 'data(value)',
-	//         opacity: 1
-	//       }
-	//     }
-	//   ],
-	//   elements: {
-	//     nodes,
-	//     links
-	//   },
-	//   layout: {
- //        name: 'concentric',
- //        concentric: function( node ){
- //          return node.degree();
- //        },
- //        levelWidth: function( nodes ){
- //          return 2;
- //        }
- //      },
-
-	// });
 }
 
 function htmlEncode(text) {
-	return $('<div></div>').text(text).html();
+	return $('<div/>').text(text).html();
 }
 
 function getContractsAmount(contracts) {
-	return contracts.reduce((last, actual) => {return last + +actual.amount}, 0).toFixed(2);
+	return contracts.reduce((totalAmount, {amount}) => totalAmount + amount, 0).toFixed(0);
 }
 
 function getContractTypes(contracts) {
@@ -241,9 +253,8 @@ function getContractsByTypes(contracts) {
 }
 
 function objectToArray(obj) {
-	const arr = [];
-	for(let i in obj) {
-		arr.push(obj[i])
-	}
-	return arr;
+	return Object.keys(obj).map(key => obj[key]);
 }
+
+
+
